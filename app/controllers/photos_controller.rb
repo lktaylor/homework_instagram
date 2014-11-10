c6beee5b10e9f39c4bbc8254ef1965cd1538bd06
Lindsay Taylor <ltaylor2@chicagobooth.edu>
2014-11-10 17:14:33 -0600

    Edit form updates

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,23 @@ class PhotosController < ApplicationController
 
   def new_form
     @photo = Photo.new_photo_form
+  end
 
+  def destroy
+    photo = Photo.find{:id}
+    photo.destroy
+  end
+
+  def edit_form
+      @photo = Photo.find_by({:id => params[:id]})
+      @photo.caption
+      @picture = @photo.source
+  end
+
+  def update_row
+      photo = Photo.find{:id}
+      photo.caption = "the_caption"
+      photo.save
   end
 
 end
